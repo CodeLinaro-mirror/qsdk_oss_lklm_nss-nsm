5001d7ea4bc5d0215856e68d185e1ecc28b24611
John Sanli <quic_jsanli@quicinc.com>
2024-10-03 16:48:40 -0700

    [qca-nss-fls] Use Relay FS for Kernel->Userspace Communication with IFLI
    Change-Id: I9e83e6ca3c09e1cd466fa177d4c2e1f74f3fea81
    Signed-off-by: John Sanli <quic_jsanli@quicinc.com>

---
+++ fls/fls_rfs.c
@@ -0,0 +1,205 @@
+/*
+ **************************************************************************
+ * Copyright (c) 2023-2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ **************************************************************************
+ */
+
+#define FLS_RFS_EVENT_MAX 128
+#define FLS_RFS_EVENT_MASK (FLS_RFS_EVENT_MAX - 1)
+
+#include <linux/module.h>
+#include <linux/ktime.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/poll.h>
+#include <linux/version.h>
+#include <linux/delay.h>
+
+#include "fls_debug.h"
+#include "fls_rfs.h"
+#include "fls_conn.h"
+
+DEFINE_SPINLOCK(fls_conn_lock);
+
+struct fls_event_log {
+	uint32_t read_index;
+	uint32_t write_index;
+	struct fls_event event_ring_buf[FLS_RFS_EVENT_MAX];
+	spinlock_t read_lock;
+	spinlock_t write_lock;
+};
+
+static struct fls_rfs rfs;
+
+struct delayed_work fls_rfs_work;
+struct workqueue_struct * fls_rfs_workqueue;
+static struct fls_event_log event_log;
+static char buf[sizeof(struct fls_rfs_telemetry_agent_header) + sizeof(struct fls_event)];
+
+void fls_rfs_write(struct work_struct *work) {
+	unsigned long irqflags;
+
+	spin_lock_irqsave(&event_log.read_lock, irqflags);
+
+	if (relay_buf_full(rfs.rbuf)) {
+		FLS_TRACE("RFS Buffer is Full, did not write\n");
+		goto queue_work;
+	}
+
+	if (event_log.read_index == event_log.write_index) {
+		spin_unlock_irqrestore(&event_log.read_lock, irqflags);
+		FLS_ERROR("Event log is empty. read_index:%d, write_index:%d\n", event_log.read_index, event_log.write_index);
+		return;
+	}
+
+	memcpy(buf + sizeof(struct fls_rfs_telemetry_agent_header), &(event_log.event_ring_buf[event_log.read_index]), sizeof(struct fls_event));
+	event_log.read_index = (event_log.read_index + 1) & FLS_RFS_EVENT_MASK;
+
+	relay_write(rfs.rfschan , &buf, sizeof(buf));
+	relay_flush(rfs.rfschan);
+	FLS_TRACE("Wrote once\n");
+
+queue_work:
+	/*
+	 * In the cases where we are writing events faster than userpace can read
+	 * we should continue to queue work, so to ensure that we will attempt
+	 * to write again and aren't dependent on the enqueue() event
+	 */
+	if (event_log.read_index != event_log.write_index) {
+		queue_delayed_work(fls_rfs_workqueue, &fls_rfs_work, FLS_RFS_WRITE_DELAY);
+	}
+
+	spin_unlock_irqrestore(&event_log.read_lock, irqflags);
+}
+
+bool fls_rfs_enqueue(struct fls_event *event)
+{
+	unsigned long irqflags;
+	uint32_t write_index;
+
+	FLS_INFO("FID: enqueue flow event.");
+	fls_debug_print_event_info(event);
+
+	spin_lock_irqsave(&event_log.write_lock, irqflags);
+	if (((event_log.write_index + 1) & FLS_RFS_EVENT_MASK) == event_log.read_index) {
+		spin_unlock_irqrestore(&event_log.write_lock, irqflags);
+		return false;
+	}
+
+	write_index = event_log.write_index;
+	event_log.event_ring_buf[write_index] = *event;
+	event_log.write_index = (write_index + 1) & FLS_RFS_EVENT_MASK;
+	spin_unlock_irqrestore(&event_log.write_lock, irqflags);
+
+	FLS_INFO("Enqeued flow event at index [%u]", write_index);
+
+	/*
+	 * Only queue write task if there is not a write task already queued
+	 */
+	if (!delayed_work_pending(&fls_rfs_work)) {
+		queue_delayed_work(fls_rfs_workqueue, &fls_rfs_work, FLS_RFS_WRITE_DELAY);
+		FLS_TRACE("Queued work to write to RFS\n");
+	} else {
+		FLS_TRACE("Did not queue RFS write work due to existing work in queue\n");
+	}
+
+	return true;
+}
+
+static void fls_rfs_tele_agent_header_fill(struct fls_rfs_telemetry_agent_header *tah)
+{
+	memset(tah, 0, sizeof(struct fls_rfs_telemetry_agent_header));
+	tah->stats_type = FLS_RFS_TELEMETRY_DATA_TYPE;
+	tah->payload_len = sizeof(struct fls_event);
+
+	memcpy(buf, tah, sizeof(struct fls_rfs_telemetry_agent_header));
+}
+
+void fls_rfs_shutdown(void)
+{
+	if (rfs.rfschan) {
+		relay_close(rfs.rfschan);
+	}
+
+	debugfs_remove_recursive(rfs.de);
+	rfs.de = NULL;
+
+	cancel_delayed_work_sync(&fls_rfs_work);
+	destroy_workqueue(fls_rfs_workqueue);
+}
+
+static int fls_rfs_remove_buf_file_handler(struct dentry *dentry)
+{
+	debugfs_remove(dentry);
+	return 0;
+}
+
+static struct dentry *fls_rfs_create_buf_file_handler(const char *filename,
+		struct dentry *parent,
+		umode_t mode,
+		struct rchan_buf *buf,
+		int *is_global)
+{
+	struct dentry *buf_file;
+
+	buf_file = debugfs_create_file(filename, mode, parent, buf,
+			&relay_file_operations);
+	if (IS_ERR(buf_file))
+		return NULL;
+
+	rfs.rbuf = buf;
+
+	*is_global = 1;
+	return buf_file;
+}
+
+static struct rchan_callbacks fls_rfs_telemetry_agent_cb = {
+	.create_buf_file = fls_rfs_create_buf_file_handler,
+	.remove_buf_file = fls_rfs_remove_buf_file_handler,
+};
+
+int fls_rfs_init(void)
+{
+	struct fls_rfs_telemetry_agent_header tah;
+
+	spin_lock_init(&event_log.read_lock);
+	spin_lock_init(&event_log.write_lock);
+
+	rfs.de = debugfs_create_dir(FLS_RFS_NAME, NULL);
+	if (rfs.de == NULL)
+		return -EPERM;
+
+	rfs.rfschan = relay_open("fls_ifli",
+		rfs.de,
+		(sizeof(struct fls_rfs_telemetry_agent_header) + sizeof(struct fls_event)),
+		 1, &fls_rfs_telemetry_agent_cb, NULL);
+	if (!rfs.rfschan) {
+		debugfs_remove_recursive(rfs.de);
+		rfs.de = NULL;
+		return -EPERM;
+	}
+
+	fls_rfs_workqueue = create_singlethread_workqueue("fls_rfs_workqueue");
+	if(!fls_rfs_workqueue) {
+		FLS_WARN("Failed to initialize FLS RFS workqueue\n");
+		return false;
+	}
+
+	fls_rfs_tele_agent_header_fill(&tah);
+
+	INIT_DELAYED_WORK(&fls_rfs_work, fls_rfs_write);
+
+	return 0;
+}
---
+++ fls/fls_rfs.h
@@ -0,0 +1,133 @@
+/*
+ **************************************************************************
+ * Copyright (c) 2023-2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ **************************************************************************
+ */
+
+#ifndef FLS_RFS_H
+#define FLS_RFS_H
+
+#include <linux/cdev.h>
+#include <linux/relay.h>
+#include <linux/workqueue.h>
+#include <linux/debugfs.h>
+#include <linux/types.h>
+#include "fls_def_sensor.h"
+
+#define FLS_RFS_NAME "fls"
+#define FLS_RFS_SAMPLES_MAX 10
+#define FLS_RFS_WINDOWS_MAX 3
+#define FLS_RFS_EVENTS_LIMIT 5
+#define FLS_RFS_TELEMETRY_DATA_TYPE 2
+#define FLS_RFS_WRITE_DELAY msecs_to_jiffies(50)
+
+enum FLS_PROTOCOL_TYPE
+{
+    UDP,
+    TCP
+};
+
+struct fls_rfs {
+	struct rchan *rfschan;
+	struct rchan_buf *rbuf;
+	struct dentry *de;
+};
+
+
+enum fls_rfs_event_types {
+	FLS_RFS_EVENT_TYPE_DEF,
+	FLS_RFS_EVENT_TYPE_XL /* Extra large window event type */
+};
+
+struct fls_def_event_window {
+	uint32_t orig_packets;
+	uint32_t orig_bytes;
+	uint32_t orig_bytes_min;
+	uint32_t orig_bytes_max;
+	uint64_t orig_delta_sum;
+	uint64_t orig_delta_min;
+	uint64_t orig_delta_max;
+	uint32_t orig_bursts;
+	uint32_t orig_burst_sz_sum;
+	uint32_t orig_burst_sz_min;
+	uint32_t orig_burst_sz_max;
+	uint64_t orig_burst_dur_sum;
+	uint64_t orig_burst_dur_min;
+	uint64_t orig_burst_dur_max;
+
+	uint32_t ret_packets;
+	uint32_t ret_bytes;
+	uint32_t ret_bytes_min;
+	uint32_t ret_bytes_max;
+	uint64_t ret_delta_sum;
+	uint64_t ret_delta_min;
+	uint64_t ret_delta_max;
+	uint32_t ret_bursts;
+	uint32_t ret_burst_sz_sum;
+	uint32_t ret_burst_sz_min;
+	uint32_t ret_burst_sz_max;
+	uint64_t ret_burst_dur_sum;
+	uint64_t ret_burst_dur_min;
+	uint64_t ret_burst_dur_max;
+};
+
+struct fls_def_event_sample
+{
+	struct fls_def_event_window window[FLS_RFS_WINDOWS_MAX];
+};
+
+struct fls_def_event {
+	uint32_t window_length[FLS_RFS_WINDOWS_MAX];
+	uint32_t sample_count;
+	struct fls_def_event_sample samples[FLS_RFS_SAMPLES_MAX];
+};
+
+struct fls_event {
+	uint8_t event_type;
+	uint8_t dir;
+
+	uint8_t ip_version;
+	uint8_t protocol;
+
+	uint16_t orig_src_port;
+	uint16_t orig_dest_port;
+	uint32_t orig_src_ip[4];
+	uint32_t orig_dest_ip[4];
+
+	uint16_t ret_src_port;
+	uint16_t ret_dest_port;
+	uint32_t ret_src_ip[4];
+	uint32_t ret_dest_ip[4];
+
+	ktime_t timestamp;
+
+	union {
+		struct fls_def_event def_event;
+	};
+};
+
+struct fls_rfs_telemetry_agent_header
+{
+	u_int32_t   start_magic_num;
+	u_int32_t   stats_version;
+	u_int32_t   stats_type;
+	u_int32_t   payload_len;
+} __attribute__ ((__packed__));
+
+bool fls_rfs_enqueue(struct fls_event *event);
+void fls_rfs_shutdown(void);
+int fls_rfs_init(void);
+
+#endif
